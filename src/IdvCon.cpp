8347a04310998df260992ad442fde8e60bd6bbef
Chun Kun Wang <amos@classroom.cs.unc.edu>
2017-10-25 22:37:29 -0400

    Create the initial files

---
+++ src/IdvCon.cpp
@@ -0,0 +1,12 @@
+#include "IdvCon.h"
+
+#include <iostream>
+
+using namespace std;
+
+namespace IdvConSpace {
+  void IdvCon::setInName(string input_string) {
+    input_name = input_string;
+  }
+}
+
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "IdvCon.h"
+
+using namespace std;
+
+int main()
+{
+  cout << "find_political_donors: Hello World!" << endl;
+  return 0;
+}
+
